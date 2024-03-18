49dce20d4036a46d1cb6b2ef983f3dd299e9ab2d
abenhoura <>
2024-03-19 00:36:19 +0100

    rename result and remove somme comments

--- src/main/java/com/sipios/refactoring/controller/ShoppingController.java
+++ src/main/java/com/sipios/refactoring/controller/ShoppingController.java
@@ -24,45 +24,34 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/shopping")
 public class ShoppingController {
 
-    private Logger logger = LoggerFactory.getLogger(ShoppingController.class);
-
     @Autowired
     private Clock clock;
 
     @PostMapping
     public String getPrice(@RequestBody Body b) {
-        double p = 0;
-        double d;
 
-        LocalDate localDate = LocalDate.now(clock);
-        Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
-        cal.setTime(date);
+        if (b.getItems() == null) {
+            return "0";
+        }
 
+        double result = 0;
+        Calendar cal = getCalendar();
         var currentCustomerType = getCurrentCustomerType(b);
 
-        // Compute total amount depending on the types and quantity of product and
-        // if we are in winter or summer discounts periods
         if (
             !isDiscountPeriod(cal)
         ) {
-            if (b.getItems() == null) {
-                return "0";
-            }
-
             for (int i = 0; i < b.getItems().length; i++) {
                 Item it = b.getItems()[i];
 
                 if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * currentCustomerType.getFactor();
+                    result += 30 * it.getNb() * currentCustomerType.getFactor();
                 } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * currentCustomerType.getFactor();
+                    result += 50 * it.getNb() * currentCustomerType.getFactor();
                 } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * currentCustomerType.getFactor();
+                    result += 100 * it.getNb() * currentCustomerType.getFactor();
                 }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
+
             }
         } else {
             if (b.getItems() == null) {
@@ -73,41 +62,46 @@ public class ShoppingController {
                 Item it = b.getItems()[i];
 
                 if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * currentCustomerType.getFactor();
+                    result += 30 * it.getNb() * currentCustomerType.getFactor();
                 } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * 0.8 * currentCustomerType.getFactor();
+                    result += 50 * it.getNb() * 0.8 * currentCustomerType.getFactor();
                 } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * 0.9 * currentCustomerType.getFactor();
+                    result += 100 * it.getNb() * 0.9 * currentCustomerType.getFactor();
                 }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
             }
         }
 
         try {
             if (b.getType().equals("STANDARD_CUSTOMER")) {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
+                if (result > 200) {
+                    throw new Exception("Price (" + result + ") is too high for standard customer");
                 }
             } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
-                if (p > 800) {
-                    throw new Exception("Price (" + p + ") is too high for premium customer");
+                if (result > 800) {
+                    throw new Exception("Price (" + result + ") is too high for premium customer");
                 }
             } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
-                if (p > 2000) {
-                    throw new Exception("Price (" + p + ") is too high for platinum customer");
+                if (result > 2000) {
+                    throw new Exception("Price (" + result + ") is too high for platinum customer");
                 }
             } else {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
+                if (result > 200) {
+                    throw new Exception("Price (" + result + ") is too high for standard customer");
                 }
             }
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
 
-        return String.valueOf(p);
+        return String.valueOf(result);
+    }
+
+    private Calendar getCalendar() {
+        LocalDate localDate = LocalDate.now(clock);
+        Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        cal.setTime(date);
+        return cal;
     }
 
     private static boolean isDiscountPeriod(Calendar cal) {
