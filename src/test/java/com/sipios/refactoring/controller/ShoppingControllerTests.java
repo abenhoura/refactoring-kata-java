e64ad3af1c2a613d2ec167be0551cde75bbed5c5
abenhoura <>
2024-03-19 00:12:03 +0100

    create first tests witch test different types and thresholds

--- src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -4,6 +4,7 @@ import com.sipios.refactoring.UnitTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.springframework.web.server.ResponseStatusException;
 
 class ShoppingControllerTests extends UnitTest {
 
@@ -11,9 +12,107 @@ class ShoppingControllerTests extends UnitTest {
     private ShoppingController controller;
 
     @Test
-    void should_not_throw() {
-        Assertions.assertDoesNotThrow(
-            () -> controller.getPrice(new Body(new Item[] {}, "STANDARD_CUSTOMER"))
-        );
+    void shoudReturnZero() {
+        //given
+        //when
+        var sut = controller.getPrice(new Body(new Item[]{}, "STANDARD_CUSTOMER"));
+        //then
+        Assertions.assertEquals("0.0", sut);
+    }
+
+    @Test
+    void shoudReturn180WhenStandarCustomer() {
+        //given
+        var items = new Item[]{
+            new Item("TSHIRT", 1),
+            new Item("DRESS", 1),
+            new Item("JACKET", 1)
+        };
+        //when
+        var sut = controller.getPrice(new Body(items, "STANDARD_CUSTOMER"));
+        //then
+        Assertions.assertEquals("180.0", sut);
+    }
+
+
+    @Test
+    void shoudReturn175WhenPremiumCustomer() {
+        //given
+        var items = new Item[]{
+            new Item("TSHIRT", 1),
+            new Item("DRESS", 1),
+            new Item("JACKET", 1)
+        };
+        //when
+        var sut = controller.getPrice(new Body(items, "PREMIUM_CUSTOMER"));
+        //then
+        Assertions.assertEquals("162.0", sut);
+    }
+
+    @Test
+    void shoudReturn175WhenPremiumCustomerWith2Dress() {
+        //given
+        var items = new Item[]{
+            new Item("TSHIRT", 2),
+            new Item("DRESS", 3),
+            new Item("JACKET", 1)
+        };
+        //when
+        var sut = controller.getPrice(new Body(items, "PREMIUM_CUSTOMER"));
+        //then
+        Assertions.assertEquals("279.0", sut);
+    }
+
+    @Test
+    void shoudReturn90WhenPlatiniumCustomer() {
+        //given
+        var items = new Item[]{
+            new Item("TSHIRT", 1),
+            new Item("DRESS", 1),
+            new Item("JACKET", 1)
+        };
+        //when
+        var sut = controller.getPrice(new Body(items, "PLATINUM_CUSTOMER"));
+        //then
+        Assertions.assertEquals("90.0", sut);
+    }
+
+    @Test
+    void shoudThrowExceptionWhenThresholdReachedStandardCustomer() {
+        //given
+        var items = new Item[]{
+            new Item("JACKET", 11)
+        };
+        //then
+        var thrown = Assertions.assertThrows(ResponseStatusException.class,
+            ()->controller.getPrice(new Body(items, "STANDARD_CUSTOMER")));
+        Assertions.assertEquals("Price (1100.0) is too high for standard customer", thrown.getReason());
+
+    }
+
+    @Test
+    void shoudThrowExceptionWhenThresholdReachedPremiumCustomer() {
+        //given
+        var items = new Item[]{
+            new Item("JACKET", 11)
+        };
+        //then
+        var thrown = Assertions.assertThrows(ResponseStatusException.class,
+            ()->controller.getPrice(new Body(items, "PREMIUM_CUSTOMER")));
+        Assertions.assertEquals("Price (990.0) is too high for premium customer", thrown.getReason());
+
+    }
+
+    @Test
+    void shoudThrowExceptionWhenThresholdReachedPlatinumCustomer() {
+        //given
+        var items = new Item[]{
+            new Item("JACKET", 200)
+        };
+        //then
+        var thrown = Assertions.assertThrows(ResponseStatusException.class,
+            ()->controller.getPrice(new Body(items, "PLATINUM_CUSTOMER")));
+        Assertions.assertEquals("Price (10000.0) is too high for platinum customer", thrown.getReason());
+
     }
 }
